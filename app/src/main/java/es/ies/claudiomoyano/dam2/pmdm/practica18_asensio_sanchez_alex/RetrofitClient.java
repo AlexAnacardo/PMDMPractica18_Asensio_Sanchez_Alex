84455e6e0173983b16abe8f83ecc9d09d6b33dd3
Alex <alexasensio04clase@gmail.com>
2026-01-15 10:33:07 +0100

    Añadido interceptor Http

--- app/src/main/java/es/ies/claudiomoyano/dam2/pmdm/practica18_asensio_sanchez_alex/RetrofitClient.java
+++ app/src/main/java/es/ies/claudiomoyano/dam2/pmdm/practica18_asensio_sanchez_alex/RetrofitClient.java
@@ -1,5 +1,7 @@
 package es.ies.claudiomoyano.dam2.pmdm.practica18_asensio_sanchez_alex;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -10,9 +12,16 @@ public class RetrofitClient{
     private static Retrofit retrofit;
 
     public static Retrofit obtenerInstancia() {
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient clienteHttp = new OkHttpClient.Builder().addInterceptor(loggingInterceptor).build();
+
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(clienteHttp)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
